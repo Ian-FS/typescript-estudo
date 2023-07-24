f0e42c63aa717f8ec8057de0f8b773cbaabd39c2
Ian_França <iiann_2009@hotmail.com>
2023-07-24 19:30:10 -0300

    corrigido falha que permitia a alteração da variavel provada do tipo Date atraves de um dos seus metodos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,29 @@
 export class Negociacao {
-    private _data:Date;
-    private _quantidade:number;
-    private _valor:number;
+    
 
-    constructor(data:Date, quantidade:number, valor:number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date,
+        private _quantidade: number,
+        private _valor: number
+    ){}
 
     get data(): Date {
-        return this._data;
+        const data = new Date(this._data.getTime())
+        return data;
     }
 
     get quantidade(): number {
-        return this._quantidade;
+        const quantidade = this._quantidade
+        return quantidade;
     }
 
     get valor(): number {
-        return this._valor;
+        const valor = this._valor
+        return valor;
     }
 
     get volume(): number {
-        return this._quantidade * this._valor;
+        const volume = this._quantidade * this._valor
+        return volume;
     }
 }
