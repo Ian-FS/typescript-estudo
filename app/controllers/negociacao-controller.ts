0b5eee3a62deb18ed98bb457c8a80b1f9c1f2a03
Ian_França <iiann_2009@hotmail.com>
2023-08-05 03:56:18 -0300

    corrigido endereço do arquivo dias da semana

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -2,7 +2,7 @@ import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesView } from "../views/negociacoes-views.js";
 import { MensagemView } from '../views/mensagem-view.js';
-import { DiasDaSemana } from '../enums/dias-da-semana';
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 
 export class NegociacaoController{
 
